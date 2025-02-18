634e780b50b2e7826fc95b233f56fc6908c48249
samizoda8 <167873586+samizoda8@users.noreply.github.com>
2025-02-17 21:19:19 -0500

    Update settings.py

--- weatherapp/settings.py
+++ weatherapp/settings.py
@@ -79,14 +79,11 @@ WSGI_APPLICATION = 'weatherapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER':config('DB_USER'),
-        'PASSWORD':config('DB_PASSWORD'),
-        'HOST':config('DB_HOST'),
-        'PORT':config('DB_PORT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
